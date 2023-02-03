8e8dcb0f402e533496a1aed29500b266dc846975
Tony Miller <firstdoubletripledev@gmail.com>
2023-02-02 20:20:16 -0600

    completed within the tutorial

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -7,14 +7,20 @@
  * 4. Copy the config object snippet, then add it here.
  */
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+    apiKey: "AIzaSyChwYSIK3WXr_xJm3gFqk8lAJS6NKxTZrk",
+    authDomain: "friendlychat-afc4a.firebaseapp.com",
+    projectId: "friendlychat-afc4a",
+    storageBucket: "friendlychat-afc4a.appspot.com",
+    messagingSenderId: "22193482612",
+    appId: "1:22193482612:web:183b458a266f3817351a21",
+    measurementId: "G-HGPVZPB164"
 };
 
 export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
-  } else {
-    return config;
-  }
+    if (!config || !config.apiKey) {
+        throw new Error('No Firebase configuration object provided.' + '\n' +
+            'Add your web app\'s configuration object to firebase-config.js');
+    } else {
+        return config;
+    }
 }
